6872a87b3790aacd66f1024c7e4dd62f74ff7407
Jacob Zhou <jacobz@yeah.net>
2012-10-18 12:30:13 +0800

    fix

--- lib/seo_helper/railtie.rb
+++ lib/seo_helper/railtie.rb
@@ -1,7 +1,7 @@
 require "seo_helper/helper"
 
 module SeoHelper
-  class Railtie < Rails::Railtie
+  class Railtie < ::Rails::Railtie
     initializer "seo_helper.view_helpers" do
       ActionView::Base.send :include, SeoHelper::Helper
     end
